e136eb9f3d3ad904bf2e44f2b22e229fa935e7ca
Константин Лихачёв <constantine.lord.darkness@gmail.com>
2019-09-14 09:31:11 +0300

    - initial project

---
+++ Cocktail DB/Cocktail DB/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Cocktail DB
+//
+//  Created by Constantine Likhachov on 9/14/19.
+//  Copyright © 2019 Constantine Likhachov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
